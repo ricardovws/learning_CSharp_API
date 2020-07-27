554a074c817de46e730db4b36ed3a88aa8620e34
Ricardo <ricardovws@gmail.com>
2020-07-27 14:01:52 -0300

    testes com postman

--- Alura.WebAPI.ProjetoInicial/Alura.WebAPI.WebApp/API/LivrosController.cs
+++ Alura.WebAPI.ProjetoInicial/Alura.WebAPI.WebApp/API/LivrosController.cs
@@ -28,5 +28,49 @@ namespace Alura.WebAPI.WebApp.API
 
             return Json(model.ToModel());
         }
+
+        [HttpPost]
+        public IActionResult Incluir([FromBody]LivroUpload model)
+        {
+            if(ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Incluir(livro);
+                var uri = Url.Action("Recuperar", new { id = livro.Id });
+                return Created(uri, livro);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public IActionResult Alterar([FromBody]LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repo.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+                _repo.Alterar(livro);
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public IActionResult Remover(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _repo.Excluir(model);
+            return NoContent();
+        }
     }
 }
